1c4b5a0bc3d021ad288460ac844d377b9295d1a5
Hazzari <free2mmm@gmail.com>
2021-12-01 00:42:54 +0300

    fix: fix slash url address

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -2158,7 +2158,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "config": () => (/* binding */ config)
 /* harmony export */ });
 const config = {
-  URL_API: 'http://ec2-18-117-110-164.us-east-2.compute.amazonaws.com',
+  URL_API: 'http://ec2-18-222-114-183.us-east-2.compute.amazonaws.com/',
 }
 
 
--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const config = {
-  URL_API: 'http://ec2-18-117-110-164.us-east-2.compute.amazonaws.com',
+  URL_API: 'http://ec2-18-222-114-183.us-east-2.compute.amazonaws.com/',
 }
